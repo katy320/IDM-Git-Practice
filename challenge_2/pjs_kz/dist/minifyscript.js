e660c45be62ba5ebfa0c7fdfd320a06e6bb500b1
Katie Zhao <katy320@gmail.com>
2018-07-30 17:26:42 -0400

    kz challenge_2 done

--- challenge_2/pjs_kz/dist/minifyscript.js
+++ challenge_2/pjs_kz/dist/minifyscript.js
@@ -1,10 +1,11 @@
-#Title
-
 const minify = require('minify');
+const fs = require('fs'); //file systems: module for reading/writing new files
 
-minify('client.js', (error, data) => {
+minify('styles.css', (error, data) => { //the file gets minified to data
 if (error)
 return console.error(error.message);
 
-console.log(data);
+fs.writeFile("newStyles.css", data, function(err){
+    console.log(err); //logs the error to the console
+    });
 });
