8fe3421095fda310fa1f47e89a778e363664ab81
Katie Zhao <katy320@gmail.com>
2018-08-02 10:55:21 -0400

    kz_beta challenge 4

---
+++ challenge_4/kz_beta/README.md
@@ -0,0 +1,6 @@
+1. Type 'npm install' in the terminal while in the main directory, kz_beta
+2. First: npm install --save-deve gulp-stylus
+3. Then: type 'gulp' in the terminal, which will run the process
+4. Files will be placed in the newly created 'dist' folder
+
+5. Also: npm install browser-sync gulp --save-dev allows the browser to sync and update by automatically refreshing when there is a change to the webpage file
---
+++ challenge_4/kz_beta/gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('css', () => {
+  return gulp.src('src/stylus/screen.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('html', () => {
+  return gulp.src('src/views/index.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['css','html']);
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+});
